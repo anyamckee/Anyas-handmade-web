f3791a34ec035b48b3c33487de5a7470327528e7
anyamckee <mckeeak@vcu.edu>
2021-07-05 22:34:22 -0400

    brrberries

---
+++ projects/blueberries/functions.js
@@ -0,0 +1,10 @@
+function cold() {
+  var bg = document.getElementById("bg");
+  bg.classList.toggle("activebg");
+  var ribs = document.getElementById("ribs");
+  ribs.classList.toggle("activeribs");
+  var ribs2 = document.getElementById("ribs2");
+  ribs2.classList.toggle("activeribs2");
+  var berry = document.getElementById("berry");
+  berry.classList.toggle("activeberry");
+}
